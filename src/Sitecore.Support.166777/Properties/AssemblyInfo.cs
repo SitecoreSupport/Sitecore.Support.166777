231ab29089b2680b8461f6c3c115f72a1b9a0764
DK\psk <psk@sitecore.net>
2017-06-08 18:49:57 -0400

    Intitial commit with OOB ItemBucket.js file

---
+++ src/Sitecore.Support.166777/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.166777")]
+[assembly: AssemblyProduct("Sitecore.Support.166777")]
+[assembly: ComVisible(false)]
